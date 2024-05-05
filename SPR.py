c8389d190ef9e9bb6d83704c0267a3034128f703
ameliatodd2002 <ameliatodd2002@gmail.com>
2024-05-05 16:13:23 -0500

    Exploratory Data Analysis performed on dataset from food aggregator company to create actionable insights and recommendations for the company to optimize sales.
    Description
    Context
    The number of restaurants in New York is increasing day by day. Lots of students and busy professionals rely on those restaurants due to their hectic lifestyles. Online food delivery service is a great option for them. It provides them with good food from their favorite restaurants. A food aggregator company FoodHub offers access to multiple restaurants through a single smartphone app.
    
    The app allows the restaurants to receive a direct online order from a customer. The app assigns a delivery person from the company to pick up the order after it is confirmed by the restaurant. The delivery person then uses the map to reach the restaurant and waits for the food package. Once the food package is handed over to the delivery person, he/she confirms the pick-up in the app and travels to the customer's location to deliver the food. The delivery person confirms the drop-off in the app after delivering the food package to the customer. The customer can rate the order in the app. The food aggregator earns money by collecting a fixed margin of the delivery order from the restaurants.
    
    Objective
    The food aggregator company has stored the data of the different orders made by the registered customers in their online portal. They want to analyze the data to get a fair idea about the demand of different restaurants which will help them in enhancing their customer experience. Suppose you are hired as a Data Scientist in this company and the Data Science team has shared some of the key questions that need to be answered. Perform the data analysis to find answers to these questions that will help the company to improve the business.
    
    Data Description
    The data contains the different data related to a food order. The detailed data dictionary is given below.

---
+++ SPR.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 00:48:39 2022
+
+@author: Amelia Todd
+
+Instructions:
+    #type in command line pip install censusgeocode
+"""
+
+import censusgeocode as cg
+#censusgeocode takes csv input with adresses and provides a new csv with previous information and a census tract number for each address
+import pandas as pd
+
+
+ogPath = 'data/addresses.csv'
+#path to find input csv file
+resPath = 'data/results.csv'
+#path to put result csv file from censusgeocode
+batchSize = 9999
+#amount given to censusgeocode at a time
+
+def batchRes(path):
+    #program starts by passing ogpath to this function as path
+    res = cg.addressbatch(path)
+    #using function created below to send csv file input to census geocode and recieve output
+    #uses ogpath as argument
+    add = []
+    #initializing list for addresses
+    tr = []
+    #initializing list for censustract
+    sf = []
+    #initializing list for state code
+    cf = []
+    #initializing list for county code
+    b = []
+    #initializing list for block code
+    
+    for r in res:
+        #going through each adress and appending each relevant column to each list
+        add.append(r['address'])
+        #appending address for each entry
+        tr.append(r['tract'])
+        #appending census tract for each entry
+        sf.append(r['statefp'])
+        #appending state code label for each entry
+        cf.append(r['countyfp'])
+        #appending county code label for each entry
+        b.append(r['block'])
+        #appending each block label for each entry
+    data = {'address': add, 'tract code': tr, 'state code': sf, 'county code': cf, 'block': b}
+    #creating a dictionary with keys for each column we want with corresponding list
+    df = pd.DataFrame(data)
+    #using pandas to create a dataframe using the dictionary created above
+    df.to_csv(resPath, mode='a')
+    #creating csv file from the dataframe and sending to result path
+    return 0
+
+def batchProcess(path):
+    #function to interact with API (censusgeocde)
+    source_path = path
+    #uses ogpath bc that is what is passed through it as argument in previous funtion
+    for i,chunk in enumerate(pd.read_csv(source_path, chunksize=batchSize)):
+        #reading csv using ogpath(source_path); defining chunksize as the batchSize defined at top of program (goes through 9999 enries at a time)
+        #using for loop to go through each entry in the csv input fie and enumerating through it to keep track of entry number
+        chunk.to_csv('data/subBatches/chunk{}.csv'.format(i), index=False)
+        #for each bath, this returns the output into a new csv file with the route defined
+        #this will create folder named data w folder inside named subBatches with outputs for each batch
+        batchRes('data/subBatches/chunk{}.csv'.format(i))
+        #passes the resulting folder back to the original function for it to sort through and organize
+    return 0
+
+if __name__ == '__main__':
+    batchProcess(ogPath)
+    #ogpath is passed through first function as an argument to provide path to find input csv file (ogpath defined at beginning)
